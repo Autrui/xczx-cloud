c29e3ba5af6d04b7ac7626e59f26838f57c606a1
Autrui <1719910793@qq.com>
2023-01-20 09:32:27 +0800

    2023.1.20 initialization

---
+++ xczx-auth/src/main/java/com/autrui/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.autrui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Autrui
+ * @date 2023/1/19
+ * @apiNote
+ */
+@SpringBootApplication
+public class AuthApplication {
+	public static void main (String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+}
---
+++ xczx-auth/src/test/java/com/autrui/classTest.java
@@ -0,0 +1,9 @@
+package com.autrui;
+
+/**
+ * @author Autrui
+ * @date 2023/1/19
+ * @apiNote
+ */
+public class classTest {
+}
